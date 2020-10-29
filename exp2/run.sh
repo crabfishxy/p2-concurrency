a116fc17109aec00bb25f7321cd94270471e7297
CrabFish <lhxx43xy@126.com>
2020-10-29 17:29:07 +0000

    update

--- exp2/run.sh
+++ exp2/run.sh
@@ -2,8 +2,9 @@
 # set -x 
 
 # TODO: Switch between the following two to disable/enable VTUNE profiling
-# VTUNE="vtune -collect hotspot -knob sampling-mode=hw"
-VTUNE=
+VTUNE="vtune -collect hotspot -knob sampling-mode=hw"
+# VTUNE="vtune -collect uarch-exploration"
+# VTUNE=
 
 # TODO: Switch among the following three for # of iterations
 #ITER=10k  # small
@@ -23,10 +24,10 @@ run() {
   
   # TODO: Set thread counts to test here
   # for tr in 1 2 4 6 8 10 12 16 20
-  for tr in 1 2 4 6 8
-  #for tr in 1 2
+  for tr in 8
+  # for tr in 20
   do 
-    $VTUNE $PROG --iterations=$ITER  --threads=$tr --parts=`expr $tr \* $FACTOR` >> $TRACEFILE 2>&1   
+    $VTUNE $PROG --iterations=$ITER  --threads=$tr --parts=`expr $FACTOR` >> $TRACEFILE 2>&1   
   done
   
   cat $TRACEFILE | grep "test="
@@ -34,28 +35,23 @@ run() {
 
 #####################
 # biglock
-run "./list" "trace-.txt" 1
+run "./hashtable" "trace-.txt" 1
 
 #####################
 # malloc
 #run "./lab2_list-malloc" "trace-malloc.txt" 1
-run "./list-p" "trace-p.txt" 1
+run "./hashtable-m" "trace-m.txt" 400
 
 #####################
 # no malloc, no stealing
 #run "./lab2_list-steal" "trace-nomalloc-nosteal.txt" 1
-run "./list-pm" "trace-pm.txt" 1
 
 #####################
 # no malloc, stealing
 #run "./lab2_list-steal" "trace-nomalloc-steal.txt" 4
-run "./list-pml" "trace-pml.txt" 4
 
-#####################
 # no malloc, stealing, padding
 # run "./lab2_list-steal-padding" "trace-nomalloc-steal-padding.txt" 4
-run "./list-pmla" "trace-pmla.txt" 4
-
 #####################
 # best
 #run "./lab2_list-steal2-naivepadding" "lab2_list-steal2-naivepadding.txt" 4
